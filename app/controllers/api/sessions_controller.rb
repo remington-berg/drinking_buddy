da6fb43ffce15cdc2dd2e9235e68fa9620cb0d0b
Remington <remingtonnberg@gmail.com>
2020-12-08 13:11:48 -0600

    adds sessions controller and create action

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,18 @@
+class Api::SessionsController < ApplicationController
+  def create
+    user = User.find_by(user_name: params[:user_name])
+    if user && user.authenticate(params[:password])
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i, # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+      render json: { jwt: jwt, user_name: user.user_name, user_id: user.id }, status: :created
+    else
+      render json: {}, status: :unauthorized
+    end
+  end
+end
