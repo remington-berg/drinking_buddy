e891d867c4d832da85153ca65904f28226c6ee39
Remington <remingtonnberg@gmail.com>
2020-12-11 11:37:23 -0600

    updates seeds

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,32 @@
+# class ApplicationController < ActionController::Base
+#   protect_from_forgery with: :exception, if: -> { request.format.html? }
+
+#   def current_user
+#     auth_headers = request.headers["Authorization"]
+#     if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+#       token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+#       begin
+#         decoded_token = JWT.decode(
+#           token,
+#           Rails.application.credentials.fetch(:secret_key_base),
+#           true,
+#           { algorithm: "HS256" }
+#         )
+#         User.find_by(id: decoded_token[0]["user_id"])
+#       rescue JWT::ExpiredSignature
+#         nil
+#       end
+#     end
+#   end
+
+#   helper_method :current_user
+
+#   def authenticate_user
+#     unless current_user
+#       render json: {}, status: :unauthorized
+#     end
+#   end
+# end
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, if: -> { request.format.html? }
 
@@ -23,7 +52,13 @@ class ApplicationController < ActionController::Base
 
   def authenticate_user
     unless current_user
-      render json: {}, status: :unauthorized
+      render json: { message: "You can't do that." }, status: :unauthorized
+    end
+  end
+
+  def authenticate_admin
+    unless current_user && current_user.admin
+      render json: { message: "Not gonna happen." }, status: :unauthorized
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
 User.create(name: "Remington", email: "remington@example.com", user_name: "remingtonnberg", password: "password")
 
 Restaurant.create(name: "Avenue Tavern", address: "2916 N Broadway Ave", phone_number: "7739757000")
+Restaurant.create(name: "Hoyt's", address: "75 E Upper Wacker Drive", phone_number: "7735555555")
 
 Special.create(name: "Happy Hour", description: "$4 well drinks and $5 drafts", day: "Monday", restaurant_id: 1)
 Special.create(name: "Happy Hour", description: "$4 well drinks and $5 drafts", day: "Tuesday", restaurant_id: 1)
 Special.create(name: "Happy Hour", description: "$4 well drinks and $5 drafts", day: "Wednesday", restaurant_id: 1)
 Special.create(name: "Happy Hour", description: "$4 well drinks and $5 drafts", day: "Thursday", restaurant_id: 1)
 Special.create(name: "Happy Hour", description: "$4 well drinks and $5 drafts", day: "Friday", restaurant_id: 1)
+Special.create(name: "Tini Tuesday", description: "$All specialty martinis 1/2 priced", day: "Tuesday", restaurant_id: 2)
